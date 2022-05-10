64145a51eade636069f8e27d037fc4a29ba8fd46
Juan Cuartas <equisoide@gmail.com>
2022-05-10 00:10:07 -0500

    Added material UI library

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,15 @@
+import Button from '@mui/material/Button';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import './styles.css';
 
 function App() {
   return (
     <div>
-      <div>React App</div>
+      <Button variant="contained">Hello World!</Button>
       <div>
         <strong>Env: </strong>
         <span>{process.env.REACT_APP_ENV}</span>
