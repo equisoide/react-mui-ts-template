8ffe8c5709d5facaf7b46b732fc7c6a8c1a10978
Juan Cuartas <equisoide@gmail.com>
2022-05-18 23:53:07 -0500

    Moved App coponent

---
+++ src/components/App/index.test.tsx
@@ -0,0 +1,6 @@
+import { render } from '@testing-library/react';
+import App from '.';
+
+test('Render App Component', () => {
+  render(<App />);
+});
---
+++ src/components/App/index.tsx
@@ -0,0 +1,11 @@
+import Copyright from '../Copyright';
+
+function App() {
+  return (
+    <div>
+      <Copyright />
+    </div>
+  );
+}
+
+export default App;
