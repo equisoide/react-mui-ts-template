a68824ed50a281eeaf20bd40c48175254255834c
Juan Cuartas <equisoide@gmail.com>
2022-06-28 23:47:44 -0500

    Added comments to HelloWorld storybook file

--- src/components/HelloWorld/index.stories.tsx
+++ src/components/HelloWorld/index.stories.tsx
@@ -1,17 +1,24 @@
+// External imports
 import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+// Local imports
 import HelloWorld from '.';
 
+// Story placement in the story list
 export default {
-  title: 'Example/HelloWorld',
+  title: 'Components/HelloWorld',
   component: HelloWorld,
 } as ComponentMeta<typeof HelloWorld>;
 
+// Template of how args map to rendering
 const Template: ComponentStory<typeof HelloWorld> = (args) => <HelloWorld {...args} />;
 
+// Story #1
 export const Green = Template.bind({});
 Green.args = {
 };
 
+// Story #2
 export const Red = Template.bind({});
 Red.args = {
   alert: { severity: 'error' },
