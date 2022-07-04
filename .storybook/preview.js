ae1239b86c8e94b3e1535b3fb817675246370a53
Juan Cuartas <equisoide@gmail.com>
2022-07-04 15:29:23 -0500

    Updated Storybook preview.js file

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,6 @@
 // Use preview.js for global code that applies to all stories.
 // Learn more: https://storybook.js.org/docs/react/configure/overview
-
-// External imports
 import { addParameters } from '@storybook/client-api';
-
-// Local imports
 import initI18n from '../src/lang';
 import '../src/styles/main.scss';
 
